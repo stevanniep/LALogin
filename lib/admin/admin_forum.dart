0471c6ed49e2a34087e6dbd7259107977e759eb0
Ahjaris <169131432+Ahjaris@users.noreply.github.com>
2025-07-25 22:48:18 +0700

    menambah presensi dan admin

---
+++ lib/admin/admin_forum.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminForumPage extends StatelessWidget {
+  const AdminForumPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Admin Forum Page')));
+  }
+}
