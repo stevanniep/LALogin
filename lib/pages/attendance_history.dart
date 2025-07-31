d736ebad484fb3851e0345dad984aef245f7802c
Dazu <158797897+DrealGea@users.noreply.github.com>
2025-07-31 16:40:36 +0700

    Add files via upload

---
+++ lib/pages/attendance_history.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AttendanceHistoryPage extends StatelessWidget {
+  const AttendanceHistoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Kontak Admin')),
+      body: const Center(child: Text('Halaman Kontak Admin')),
+    );
+  }
+}
